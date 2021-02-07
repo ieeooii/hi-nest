11d055e0b5c3c67014afa8b3dc3695d590833329
CLOwinnie <winnie@clo3d.com>
2021-02-07 14:56:34 +0900

    create-movie.dto.ts validation part one

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Injectable()
 export class MoviesService {
@@ -9,27 +10,27 @@ export class MoviesService {
     return this.movies;
   }
 
-  getOne(id: string): Movie {
-    const movie = this.movies.find((movie: Movie) => movie.id === +id);
+  getOne(id: number): Movie {
+    const movie = this.movies.find((movie: Movie) => movie.id === id);
     if (!movie) {
       throw new NotFoundException(`Movie with Id ${id}`);
     }
     return movie;
   }
 
-  deleteOne(id: string) {
+  deleteOne(id: number): void {
     this.getOne(id);
-    this.movies = this.movies.filter((movie: Movie) => movie.id !== +id);
+    this.movies = this.movies.filter((movie: Movie) => movie.id !== id);
   }
 
-  create(movieData) {
+  create(movieData: CreateMovieDto): void {
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData,
     });
   }
 
-  update(id: string, updateData) {
+  update(id: number, updateData: CreateMovieDto): void {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updateData });
